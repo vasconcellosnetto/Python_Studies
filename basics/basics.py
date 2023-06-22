404fb9119701ab2b2eb4a3028c7cee9c1cc6161e
Antonio Netto <37848607+vasconcellosnetto@users.noreply.github.com>
2023-06-22 16:03:20 -0300

    adding print and str references

--- basics/basics.py
+++ basics/basics.py
@@ -1,3 +1,13 @@
 print(1, 2, sep="-", end="\n")
-#sep é usado quando queremos alterar o separados dos argumentos da função print
-#end é usado para alterar o final da mensagem do print. Por padrão é \n
+print("Olá \"amigo\"")
+print('Olá "amigo"')
+
+'''
+• sep é usado quando queremos alterar o separados dos argumentos da função print
+• end é usado para alterar o final da mensagem do print. Por padrão é \n
+• o símbolo de escape (\) é usado quando queremos mostrar um caracter que não será interpretado
+• o símbolo de expressão regular (r) pode ser usado em casos onde é necessário usar a barra invertida
+	para exibição
+
+• no caso de aspas, basta trocar entre aspas simples e duplas para um codigo mais limpo
+'''
