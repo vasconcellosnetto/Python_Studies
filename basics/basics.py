b76bcb1e74c830a4ea91e95101df6fb03acdb8ce
Antonio Netto <37848607+vasconcellosnetto@users.noreply.github.com>
2023-06-22 15:25:56 -0300

    update basics

---
+++ basics/basics.py
@@ -0,0 +1,3 @@
+print(1, 2, sep="-", end="\n")
+#sep é usado quando queremos alterar o separados dos argumentos da função print
+#end é usado para alterar o final da mensagem do print. Por padrão é \n
